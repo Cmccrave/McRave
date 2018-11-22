390cb81523c20a27899f431a2da708b8df4951c2
Fawx <christianmccrave@gmail.com>
2018-11-21 23:07:53 -0500

    Inlined everything to remove some linker errors
    Still have some linker errors

--- Source/BWEB/BWEB.h
+++ Source/BWEB/BWEB.h
@@ -9,83 +9,84 @@
 
 namespace BWEB::Map
 {
-	BWEM::Map& mapBWEM = BWEM::Map::Instance();
+	inline BWEM::Map& mapBWEM = BWEM::Map::Instance();
 
 	// Unneeded
-	void findMain(), findMainChoke(), findNatural(), findNaturalChoke(), findNeutrals();
+	inline void findMain(), findMainChoke(), findNatural(), findNaturalChoke(), findNeutrals();
 
 	// UNSORTED BELOW THIS LINE
 	// -------------------------------------
 
 	// Map		
-	BWAPI::Position mainPosition, naturalPosition;
-	BWAPI::TilePosition mainTile, naturalTile;
-	const BWEM::Area * naturalArea{};
-	const BWEM::Area * mainArea{};
-	const BWEM::ChokePoint * naturalChoke{};
-	const BWEM::ChokePoint * mainChoke{};
-	std::set<BWAPI::TilePosition> usedTiles;
-	void addOverlap(BWAPI::TilePosition, int, int);
-	bool isPlaceable(BWAPI::UnitType, BWAPI::TilePosition);
+	inline BWAPI::Position mainPosition, naturalPosition;
+	inline BWAPI::TilePosition mainTile, naturalTile;
+	inline const BWEM::Area * naturalArea{};
+	inline const BWEM::Area * mainArea{};
+	inline const BWEM::ChokePoint * naturalChoke{};
+	inline const BWEM::ChokePoint * mainChoke{};
+	inline std::set<BWAPI::TilePosition> usedTiles;
+	inline void addOverlap(BWAPI::TilePosition, int, int);
+	inline bool isPlaceable(BWAPI::UnitType, BWAPI::TilePosition);
 
 	// General
-	int testGrid[256][256];
-	int reserveGrid[256][256] ={};
+	inline int testGrid[256][256];
+	inline int reserveGrid[256][256] ={};
+	inline int overlapGrid[256][256] ={};
+	inline int usedGrid[256][256] ={};
 
-	void draw(), onStart(), onUnitDiscover(BWAPI::Unit), onUnitDestroy(BWAPI::Unit), onUnitMorph(BWAPI::Unit);
-	int overlapGrid[256][256] ={};
-	int usedGrid[256][256] ={};
+
+	inline void draw(), onStart(), onUnitDiscover(BWAPI::Unit), onUnitDestroy(BWAPI::Unit), onUnitMorph(BWAPI::Unit);
 
 	/// This is just put here so pathfinding can use it for now
-	BWAPI::UnitType overlapsCurrentWall(BWAPI::TilePosition tile, int width = 1, int height = 1);
+	inline BWAPI::UnitType overlapsCurrentWall(BWAPI::TilePosition tile, int width = 1, int height = 1);
 
-	bool overlapsAnything(BWAPI::TilePosition here, int width = 1, int height = 1, bool ignoreBlocks = false);
-	bool isWalkable(BWAPI::TilePosition);
-	int tilesWithinArea(BWEM::Area const *, BWAPI::TilePosition here, int width = 1, int height = 1);
+	inline bool overlapsAnything(BWAPI::TilePosition here, int width = 1, int height = 1, bool ignoreBlocks = false);
+	inline bool isWalkable(BWAPI::TilePosition);
+	inline int tilesWithinArea(BWEM::Area const *, BWAPI::TilePosition here, int width = 1, int height = 1);
 
 	/// <summary> Returns the closest buildable TilePosition for any type of structure </summary>
 	/// <param name="type"> The UnitType of the structure you want to build.</param>
 	/// <param name="tile"> The TilePosition you want to build closest to.</param>
-	BWAPI::TilePosition getBuildPosition(BWAPI::UnitType type, BWAPI::TilePosition searchCenter = BWAPI::Broodwar->self()->getStartLocation());
+	inline BWAPI::TilePosition getBuildPosition(BWAPI::UnitType type, BWAPI::TilePosition searchCenter = BWAPI::Broodwar->self()->getStartLocation());
 
 	/// <summary> Returns the closest buildable TilePosition for a defensive structure </summary>
 	/// <param name="type"> The UnitType of the structure you want to build.</param>
 	/// <param name="tile"> The TilePosition you want to build closest to. </param>
-	BWAPI::TilePosition getDefBuildPosition(BWAPI::UnitType type, BWAPI::TilePosition tile = BWAPI::Broodwar->self()->getStartLocation());
+	inline BWAPI::TilePosition getDefBuildPosition(BWAPI::UnitType type, BWAPI::TilePosition tile = BWAPI::Broodwar->self()->getStartLocation());
 
 	template <class T>
 	/// <summary> Returns the estimated ground distance from one Position type to another Position type.</summary>
 	/// <param name="first"> The first Position. </param>
 	/// <param name="second"> The second Position. </param>
-	double getGroundDistance(T start, T end);
+	inline double getGroundDistance(T start, T end);
 
 	/// <summary> Returns the BWEM::Area of the natural expansion </summary>
-	const BWEM::Area * getNaturalArea() { return naturalArea; }
+	inline const BWEM::Area * getNaturalArea() { return naturalArea; }
 
 	/// <summary> Returns the BWEM::Area of the main </summary>
-	const BWEM::Area * getMainArea() { return mainArea; }
+	inline const BWEM::Area * getMainArea() { return mainArea; }
 
 	/// <summary> Returns the BWEM::Chokepoint of the natural </summary>
-	const BWEM::ChokePoint * getNaturalChoke() { return naturalChoke; }
+	inline const BWEM::ChokePoint * getNaturalChoke() { return naturalChoke; }
 
 	/// <summary> Returns the BWEM::Chokepoint of the main </summary>
-	const BWEM::ChokePoint * getMainChoke() { return mainChoke; }
+	inline const BWEM::ChokePoint * getMainChoke() { return mainChoke; }
 
 	/// Returns the TilePosition of the natural expansion
-	BWAPI::TilePosition getNaturalTile() { return naturalTile; }
+	inline BWAPI::TilePosition getNaturalTile() { return naturalTile; }
 
 	/// Returns the Position of the natural expansion
-	BWAPI::Position getNaturalPosition() { return naturalPosition; }
+	inline BWAPI::Position getNaturalPosition() { return naturalPosition; }
 
 	/// Returns the TilePosition of the main
-	BWAPI::TilePosition getMainTile() { return mainTile; }
+	inline BWAPI::TilePosition getMainTile() { return mainTile; }
 
 	/// Returns the Position of the main
-	BWAPI::Position getMainPosition() { return mainPosition; }
+	inline BWAPI::Position getMainPosition() { return mainPosition; }
 
 	/// Returns the set of used TilePositions
-	std::set<BWAPI::TilePosition>& getUsedTiles() { return usedTiles; }
+	inline std::set<BWAPI::TilePosition>& getUsedTiles() { return usedTiles; }
 
 	/// Returns two positions representing a line of best fit for a given BWEM::Chokepoint
-	std::pair<BWAPI::Position, BWAPI::Position> lineOfBestFit(BWEM::ChokePoint const *);
+	inline std::pair<BWAPI::Position, BWAPI::Position> lineOfBestFit(BWEM::ChokePoint const *);
 }
