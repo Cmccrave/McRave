a19196a6f685014e3709d6512acedf3b61ca51b2
Fawx <christianmccrave@gmail.com>
2018-12-13 00:53:43 -0500

    Let Zealots attack workers again

--- Source/McRave/TargetManager.cpp
+++ Source/McRave/TargetManager.cpp
@@ -23,7 +23,7 @@ namespace McRave
 								|| (target.getAirDamage() == 0.0 && target.getGroundDamage() == 0.0);
 
 			// Zealot: Don't attack non threatening workers in our territory
-			if ((unit.getType() == UnitTypes::Protoss_Zealot && target.getType().isWorker() && !Units().isThreatening(target))
+			if ((unit.getType() == UnitTypes::Protoss_Zealot && target.getType().isWorker() && !Units().isThreatening(target) && Terrain().isInAllyTerritory(target.getTilePosition()))
 
 				// If target is an egg, larva, scarab or spell
 				|| (target.getType() == UnitTypes::Zerg_Egg || target.getType() == UnitTypes::Zerg_Larva || target.getType() == UnitTypes::Protoss_Scarab || target.getType().isSpell())
